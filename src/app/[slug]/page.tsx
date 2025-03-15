96955a9cc2a76daa2125fe0c0d8d0ca386a94084
Xamid <xamidoffical@gmail.com>
2025-03-15 21:14:59 +0500

    commm

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, ResolvingMetadata } from "next";
+import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { Key } from "react";
